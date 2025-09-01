dd36dee7a7bafa9d1c067ccb17c50f5b606adfba
Apra <apra.khanna2304@gmail.com>
2025-09-01 11:10:01 +0530

    Initial commit after cleaning node_modules

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,   // purane build ko delete karega
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',   // tumhara original html
+      filename: 'index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], // niche CSS ke liye
+      },
+    ],
+  },
+};
